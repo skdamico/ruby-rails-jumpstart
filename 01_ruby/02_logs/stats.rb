99a1e7ddc7e3a1e9cd2938ca993f99576e7dfb0a
Stefano DAmico <skdamico@gmail.com>
2012-02-19 19:13:15 -0500

    01_ruby/02_logs - Complete. All features have been implemented and commented.

--- 01_ruby/02_logs/stats.rb
+++ 01_ruby/02_logs/stats.rb
@@ -1,30 +1,70 @@
 
-filename = ARGV.shift                   # get a filename from the command line arguments
+filename = ARGV.shift                         # get a filename from the command line arguments
 
-unless filename                         # we can't work without a filename
+unless filename                               # we can't work without a filename
   puts "no filename specified!"
   exit
 end
 
-lines = 0                               # a humble line counter
-unique_users = "unknown"                # someday, this will work
-unique_pages = "unknown"                # someday, this will work
-most_active_day = "unknown"             # someday, this will work
-most_active_user = "unknown"            # someday, this will work
-most_active_page = "unknown"            # someday, this will work
+lines = 0                                     # a humble line counter
 
-open(filename).each do |m|              # loop over every line of the file
-  m.chomp!                              # remove the trailing newline
-  values = m.split(",")                 # split comma-separated fields into a values array
+                                              # default hashes at 0 for easy '+=' operation
+all_days = Hash.new(0)
+all_users = Hash.new(0)
+all_pages = Hash.new(0)
 
-  # ...
+page_views = {}                               # empty hash for pages to users viewing them
 
-  lines += 1                            # bump the counter
+open(filename).each do |m|                    # loop over every line of the file
+  m.chomp!                                    # remove the trailing newline
+
+  unless m.match(/DATE,USER_ID,URL/)          # ignore the header line
+    values = m.split(",")                     # split comma-separated fields into a values array
+
+    day, user, page = values                  # assign values to good names
+
+    all_days[day] += 1                        # get day
+    all_users[user] += 1                      # get user
+    all_pages[page] += 1                      # get page
+
+    unless page_views.has_key?(page)          # initialize empty page/user array
+      page_views[page] = []
+    end
+
+    unless page_views[page].include?(user)    # only add user to list if not added already
+      page_views[page] << user                # add users to page
+    end
+
+    lines += 1                                # bump the counter
+  end
 end
 
+                                              # do calculations based on gathered data
+unique_users = all_users.length               # returns all keys for users
+unique_pages = all_pages.length               # returns all keys for pages
+
+                                              # sort the Hash into a value sorted Array
+                                              # get the last key in the 2D array
+most_active_day = all_days.sort_by {|k,v| v}.last[0]
+most_active_user = all_users.sort_by {|k,v| v}.last[0]
+most_active_page = all_pages.sort_by {|k,v| v}.last[0]
+
+popular_page = ""
+popular_page_user_count = 0
+
+page_views.each do |page,users|               # find most unique users in each page/user map
+  count = users.count
+  if count > popular_page_user_count          # test if this unique user count is higher
+    popular_page = page                       # make this the new popular page
+    popular_page_user_count = count
+  end
+end
+
+
 puts "total lines: #{lines}"                  # output stats
-puts "unique users: #{unique_users}"          # someday, this will work
-puts "unique pages: #{unique_pages}"          # someday, this will work
-puts "most active day: #{most_active_day}"    # someday, this will work
-puts "most active user: #{most_active_user}"  # someday, this will work
-puts "most active page: #{most_active_page}"  # someday, this will work
+puts "unique users: #{unique_users}"
+puts "unique pages: #{unique_pages}"
+puts "most active day: #{most_active_day}"
+puts "most active user: #{most_active_user}"
+puts "most active page: #{most_active_page}"
+puts "most popular page: #{popular_page}"
