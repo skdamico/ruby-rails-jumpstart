42afeba48219da3df08c38a414c76904c19f3e56
Stefano DAmico <skdamico@gmail.com>
2012-02-19 21:59:19 -0500

    01_ruby/03_web - added duckdef.rb and made duck.rb accept args from the command line.

--- 01_ruby/03_web/duckdef.rb
+++ 01_ruby/03_web/duckdef.rb
@@ -11,9 +11,6 @@ BASE_URL = "http://api.duckduckgo.com/?format=json&pretty=1&q="     # remote API
 query_url = BASE_URL + URI.escape(query)                            # putting the 2 together
 
 
-puts " ======================================== "                   # fancy output
-puts "   #{query_url}"                                              # fancy output
-
 object = open(query_url) do |v|                                     # call the remote API
   input = v.read                                                    # read the full response
 #  puts input                                                       # un-comment this to see the returned JSON magic
@@ -21,17 +18,5 @@ object = open(query_url) do |v|                                     # call the r
 end
 
 puts " ======================================== "                   # fancy output
-puts "   #{object['Heading']}"
-puts "     #{object['Abstract']}"
-puts "     #{object['AbstractURL']}"
-puts " ---------------------------------------- "
-
-object['RelatedTopics'].each do |rt|                                # processing sub-elements
-  puts
-  puts "   * #{rt['Text']}"
-  puts "     #{rt['FirstURL']}"
-end
-
-puts                                                                # fancy output
-puts " ---------------------------------------- "
+puts "   #{object["Definition"]}"                                   # only print the definition
 puts " ======================================== "
